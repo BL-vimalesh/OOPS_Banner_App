7e8305b62778cf97ccfd4c052de8beac4bcf1d3b
vimalesh <vimalesh.ravikumar@bridgelabz.com>
2026-01-29 14:46:18 +0530

    Added uc1 oopsBannerApp display.

---
+++ src/OopsBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * * OOPS Banner App UC1 – OOPS Banner Display Application
+ * <p>
+ * This class demonstrates a simple Java application
+ * that displays the Object Oriented Programming System (OOPS)
+ * acronym to the console.
+ *
+ * @author User
+ * @version 1.0
+ */
+
+public class OopsBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
