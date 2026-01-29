a1c4baeec1f4f17b8881d7b96dc6c4dad4424af4
vimalesh <vimalesh.ravikumar@bridgelabz.com>
2026-01-29 14:50:20 +0530

    Added uc-2 to Render OOPS Banner using print Statement.

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,9 +1,9 @@
 /**
- * OOPSBannerApp UC2 - OOPS Banner Display Application
- * <p>
- * This class demonstrates the creation of ASCII art banner using Object-Oriented Programming principles.
- * It extends the basic banner concept by displaying the word "OOPS" in a visual format using asterisks (*)
- * and spaces to form each letter.
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * This program displays the word "OOPS" as a large banner
+ * using asterisks (*) and spaces. Each line of the banner
+ * is printed using individual print statements.
  *
  * @author User
  * @version 2.0
@@ -11,17 +11,12 @@
 
 public class OopsBannerApp {
     public static void main(String[] args) {
-        String[] banner = {
-                "  ***       ***     ******      *****",
-                "**   **  **     **  **    **   **",
-                "**   **  **     **  **     ** **",
-                "**   **  **     **  **   **    **",
-                "**   **  **     **  *****         ***",
-                "**   **  **     **  **              **",
-                "  ***       ***     **         *****"
-        };
-        for (String line : banner) {
-            System.out.println(line);
-        }
+        System.out.println("  ***      ***     ******    **** ");
+        System.out.println(" **   **  **   **   **   **  **    ");
+        System.out.println(" **   **  **   **   **   **  **    ");
+        System.out.println(" **   **  **   **   ******    **** ");
+        System.out.println(" **   **  **   **   **           **");
+        System.out.println(" **   **  **   **   **           **");
+        System.out.println("  ***      ***      **        **** ");
     }
 }
