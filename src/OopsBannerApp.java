9e9d5ad40521307e750bfce4cad87a6a4653b890
vimalesh <vimalesh.ravikumar@bridgelabz.com>
2026-01-29 14:53:48 +0530

    Added uc-3 OOPS Banner Rendering using string.join()

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,64 +1,23 @@
 /**
- * OOPSBannerApp – OOPS Banner Application (Use Case 3)
+ * OOPSBannerApp UC3 – OOPS Banner Application (Use Case 3)
  *
- * This class extends User Story 2 to display the "OOPS" banner using a modular approach
- * by implementing the Single Responsibility Principle (SRP) and Method Extraction.
- *
- * <p><b>Pattern:</b> String arrays representing ASCII art for letters O, P, and S are generated
- * and combined horizontally to create the visual "OOPS" message.</p>
+ * This class extends UC2 by using the String.join() method
+ * to construct each line of the "OOPS" banner. This approach
+ * improves memory efficiency and code readability by avoiding
+ * multiple intermediate String objects created using the +
+ * operator.
  *
  * @author User
  * @version 3.0
  */
-
 public class OopsBannerApp {
-    private static String[] getOPattern() {
-        String[] result = {
-                "  ***  ",
-                "**   **",
-                "**   **",
-                "**   **",
-                "**   **",
-                "**   **",
-                "  ***  "
-        };
-        return result;
-    }
-
-    private static String[] getPPattern() {
-        String[] result = {
-                "******",
-                "**  **",
-                "**  **",
-                "******",
-                "**    ",
-                "**    ",
-                "**    "
-        };
-        return result;
-    }
-
-    private static String[] getSPattern() {
-        String[] result = {
-                " ****",
-                "**    ",
-                "**    ",
-                " **** ",
-                "    **",
-                "    **",
-                "**** "
-        };
-        return result;
-    }
-
     public static void main(String[] args) {
-        String[] o1 = getOPattern();
-        String[] o2 = getOPattern();
-        String[] p = getPPattern();
-        String[] s = getSPattern();
-
-        for (int i = 0; i < o1.length; i++) {
-            System.out.println(o1[i] + "   " + o2[i] + "   " + p[i] + "   " + s[i]);
-        }
+        System.out.println(String.join(" ","  ***  ", "    ***  ", "******", " **** "));
+        System.out.println(String.join(" "," **   **", " **   **", "**   **", "**    "));
+        System.out.println(String.join(" "," **   **", " **   **", "**   **", "**    "));
+        System.out.println(String.join(" "," **   **", " **   **", "******", " **** "));
+        System.out.println(String.join(" "," **   **", " **   **", "**     ", "    **"));
+        System.out.println(String.join(" "," **   **", " **   **", "**     ", "    **"));
+        System.out.println(String.join(" ","  ***  ", "  ***  ", "**     ", "**** " ));
     }
 }
